1d29be44996fb398f1224f1e0efbb22b34490524
Matthew Zervas <matthewrobertzervas@gmail.com>
2026-01-26 22:36:15 -0500

    refactor currency.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod shared_types;
 pub mod shared_fns;
-pub mod settlement_currency;
+pub mod currency;
 
 pub use shared_types::*;
 pub use shared_fns::*;
-pub use settlement_currency::*;
+pub use currency::*;
